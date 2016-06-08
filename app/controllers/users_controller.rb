b2c49eef93a4e9e2227be0c9c67ec791a0e4d58c
daikwn <dai-kawano@dac.co.jp>
2016-06-08 09:00:49 +0000

    update routes error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to @user , notice: '編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
